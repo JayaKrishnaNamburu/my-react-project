dcf814cca9596e809ada13ecf23b71aac422e944
JayaKrishna <namburu.venkata@tcs.com>
2018-09-03 21:40:25 +0530

    added enzyme and jest`

---
+++ src/components/app.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
